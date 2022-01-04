446b6447d74055cbdc9be20dc9ae4deb2742c16a
Benjamin Packer <benjaminpacker16@gmail.com>
2022-01-04 14:06:49 +0000

    add task 1 and 2 compatiability

--- 2015/day04.py
+++ 2015/day04.py
@@ -1,5 +1,7 @@
 import hashlib
 
+task = 1
+zeroes = '00000' if task == 1 else '000000'
 key = ''
 
 with open('2015/data/input/day04.txt', 'r') as file:
@@ -10,7 +12,7 @@ index = 0
 while True:
     # print(f'Trying {key}{index}')
     md5_value = hashlib.md5(f'{key}{index}'.encode('utf-8')).hexdigest()
-    if md5_value[:6] == '000000':
+    if md5_value[:len(zeroes)] == zeroes:
         print(index)
         break
     index = index + 1
