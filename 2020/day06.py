333c06627275c82b00b6b57285149374e73616fd
Benjamin Packer <benjaminpacker16@gmail.com>
2022-01-14 19:26:44 +0000

    complete day 6

---
+++ 2020/day06.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+from soupsieve import match
+
+groups = []
+
+with open('2020/data/input/day06.txt') as file:
+    group_answers = []
+    for line in file:
+        line = line.strip()
+        if line != '': group_answers.append(line)
+        else:
+            group = ''.join(group_answers)
+            responses = len(group_answers)
+            answers = set(list(group))
+            matching_answers = set.intersection(*[set(list(g)) for g in group_answers])
+            groups.append((group, responses, len(answers), len(matching_answers)))
+            group_answers = []
+    group = ''.join(group_answers)
+    responses = len(group_answers)
+    answers = set(list(group))
+    matching_answers = set.intersection(*[set(list(g)) for g in group_answers])
+    groups.append((group, responses, len(answers), len(matching_answers)))
+
+task_1 = sum([g[2] for g in groups])
+print(f'Task One: {task_1}')
+
+task_2 = sum([g[-1] for g in groups])
+print(f'Task One: {task_2}')
