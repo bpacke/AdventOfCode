80be633146dee2bddc8af3fadda08914bdb06821
Benjamin Packer <benjaminpacker16@gmail.com>
2022-01-21 09:38:23 +0000

    refactor making use of funcs, not one block of code

--- 2016/day01.py
+++ 2016/day01.py
@@ -1,86 +1,69 @@
 
+def read_in_file(filename):
+    instructions = []
+    with open(filename) as file:
+        for line in file:
+            instructions += line.strip().split(', ')
+    return instructions
 
-x_travelled = 0
-y_travelled = 0
-directions = ['N', 'E', 'S', 'W']
-current_direction = 'N'
+def do_task(instructions, task_two=False):
+        x_travelled = 0
+        y_travelled = 0
+        directions = ['N', 'E', 'S', 'W']
+        current_direction = 'N'
+        visited_locations = [(0, 0)]
 
-instructions = []
+        found = False
+        index = 0
+        while found == False and index < len(instructions):
+            i = instructions[index]
+            travel_direction = i[0]
+            travel_distance = int(i[1:])
+            if travel_direction == 'R': current_direction = directions [(directions.index(current_direction) + 1) % len(directions)]
+            elif travel_direction == 'L': current_direction = directions [(directions.index(current_direction) - 1) % len(directions)]
+            if current_direction == 'N':
+                for _ in range(travel_distance):
+                    y_travelled += 1
+                    coords = (y_travelled, x_travelled)
+                    visited_locations.append(coords)
+                    if task_two and visited_locations[-1] in visited_locations[:-1]: 
+                        visited_locations.append(coords)
+                        found = True
+                        break
 
-with open('2016/data/input/day01.txt') as file:
-    for line in file:
-        instructions += line.strip().split(', ')
+            elif current_direction == 'S':
+                for _ in range(travel_distance): 
+                    y_travelled -= 1
+                    coords = (y_travelled, x_travelled)
+                    visited_locations.append(coords)
+                    if task_two and visited_locations[-1] in visited_locations[:-1]: 
+                        visited_locations.append(coords)
+                        found = True
+                        break
 
-for i in instructions:
-    travel_direction = i[0]
-    travel_distance = int(i[1:])
-    if travel_direction == 'R': current_direction = directions [(directions.index(current_direction) + 1) % len(directions)]
-    elif travel_direction == 'L': current_direction = directions [(directions.index(current_direction) - 1) % len(directions)]
+            elif current_direction == 'E':
+                for _ in range(travel_distance):
+                    x_travelled += 1
+                    coords = (y_travelled, x_travelled)
+                    visited_locations.append(coords)
+                    if task_two and visited_locations[-1] in visited_locations[:-1]: 
+                        visited_locations.append(coords)
+                        found = True
+                        break
 
-    if current_direction == 'N': y_travelled += travel_distance
-    elif current_direction == 'S': y_travelled -= travel_distance
-    elif current_direction == 'E': x_travelled += travel_distance
-    elif current_direction == 'W': x_travelled -= travel_distance
+            elif current_direction == 'W':
+                for _ in range(travel_distance):
+                    x_travelled -= 1
+                    coords = (y_travelled, x_travelled)
+                    visited_locations.append(coords)
+                    if task_two and visited_locations[-1] in visited_locations[:-1]: 
+                        visited_locations.append(coords)
+                        found = True
+                        break
+            index += 1
+        return abs(visited_locations[-1][0])  + abs(visited_locations[-1][1])
 
-total_distance = abs(x_travelled) + abs(y_travelled)
-print(f'Task One: {total_distance}')
-
-
-x_travelled = 0
-y_travelled = 0
-current_direction = 'N'
-visited_locations = [(0, 0)]
-double_visit = (0, 0)
-
-found = False
-index = 0
-while found == False and index < len(instructions):
-    i = instructions[index]
-    travel_direction = i[0]
-    travel_distance = int(i[1:])
-    if travel_direction == 'R': current_direction = directions [(directions.index(current_direction) + 1) % len(directions)]
-    elif travel_direction == 'L': current_direction = directions [(directions.index(current_direction) - 1) % len(directions)]
-
-    if current_direction == 'N':
-        for _ in range(travel_distance):
-            y_travelled += 1
-            coords = (y_travelled, x_travelled)
-            if coords not in visited_locations: visited_locations.append(coords)
-            else:
-                double_visit = coords
-                found = True
-                break
-
-    elif current_direction == 'S':
-        for _ in range(travel_distance): 
-            y_travelled -= 1
-            coords = (y_travelled, x_travelled)
-            if coords not in visited_locations: visited_locations.append(coords)
-            else:
-                double_visit = coords
-                found = True
-                break
-
-    elif current_direction == 'E':
-        for _ in range(travel_distance):
-            x_travelled += 1
-            coords = (y_travelled, x_travelled)
-            if coords not in visited_locations: visited_locations.append(coords)
-            else:
-                double_visit = coords
-                found = True
-                break
-
-    elif current_direction == 'W':
-        for _ in range(travel_distance):
-            x_travelled -= 1
-            coords = (y_travelled, x_travelled)
-            if coords not in visited_locations: visited_locations.append(coords)
-            else:
-                double_visit = coords
-                found = True
-                break
-
-    index += 1
-
-print(f'Task Two: {abs(double_visit[0])  + abs(double_visit[1])}')
+if __name__ == '__main__':
+    steps = read_in_file('2016/data/input/day01.txt')
+    print(f'Task One: {do_task(steps)}')
+    print(f'Task Two: {do_task(steps, task_two=True)}')
