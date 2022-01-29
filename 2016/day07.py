254908ae2cf34401cf470e7ce466f4e077de7334
Benjamin Packer <benjaminpacker16@gmail.com>
2022-01-29 09:51:33 +0000

    complete task 2

--- 2016/day07.py
+++ 2016/day07.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 def read_puzzle_input(filename):
     ipv7_addresses = []
     with open(filename) as file:
@@ -29,8 +30,33 @@ def task_one(puzzle_input):
             supports_TLS.append(ip)
     return supports_TLS
 
+def does_contain_ABA(in_string):
+    substrings = [in_string[s:e] for s in range(len(in_string)) for e in range(s + 1, len(in_string) + 1) if len(in_string[s:e]) == 3]
+    to_return = []
+    for s in substrings:
+        character_count = len(Counter(s).keys())
+        if character_count == 2 and list(s) == list(reversed(s)): to_return.append(s)
+    return to_return
+
+def ipv7_supports_SSL(ipv7):
+    supernets = []
+    for ip in ipv7[0]:
+        supernets.extend(does_contain_ABA(ip))
+    hypernets = []
+    for ip in ipv7[1]:
+        hypernets.extend(does_contain_ABA(ip))
+    if supernets is not [] and hypernets is not []:
+        for s in supernets:
+            for h in hypernets:
+                if s[:2] == h[1:]: return True
+    return False
+
 def task_two(puzzle_input):
-    return []
+    supports_SSL = []
+    for ip in puzzle_input:
+        if ipv7_supports_SSL(ip):
+            supports_SSL.append(ip)
+    return supports_SSL
 
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input('2016/data/input/day07.txt')
