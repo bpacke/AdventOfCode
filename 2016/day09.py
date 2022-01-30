010d3565f18102b56b93cbea8d0a854a0f78570b
Benjamin Packer <benjaminpacker16@gmail.com>
2022-01-30 00:03:38 +0000

    complete day 09

--- 2016/day09.py
+++ 2016/day09.py
@@ -1,31 +1,33 @@
-
 from re import findall, match
 
 
 def read_puzzle_input(filename):
     with open(filename) as file:
-        return [line.strip() for line in file]
+        return [line.strip() for line in file][0]
 
-def task_one(line):
-    out_string = ''
+def do_task(line, task_two):
+    out_count = 0
     index = 0
     while index < len(line):
-        m = match('\(\d+x\d+\)', line[index:])
-        if m:
-            index += m.end() - 1
-            char_count = int(findall('\d+', m.group())[0])
-            repeat_count = int(findall('\d+', m.group())[1])
-            to_repeat = (line[index + 1 : index + 1 + char_count]) * repeat_count
-            out_string += to_repeat
+        marker = match('\(\d+x\d+\)', line[index:])
+        if marker:
+            index += marker.end() - 1
+            char_count = int(findall('\d+', marker.group())[0])
+            repeat_count = int(findall('\d+', marker.group())[1])
+            if task_two:
+                to_repeat =  do_task(line[index + 1 : index + 1 + char_count], True) * repeat_count
+            else:
+                to_repeat = char_count * repeat_count
+            out_count += to_repeat
             index += char_count
         else:
-            out_string += line[index]
+            out_count += 1
         index += 1
-    return out_string
+    return out_count
 
 if __name__ == '__main__':
-    line = read_puzzle_input('2016/data/input/day09.txt')[0]
-    t1 = task_one(line)
-    while '(' in t1:
-        print(t1.count('('))
-        t1 = task_one(t1)
+    line = read_puzzle_input('2016/data/input/day09.txt')
+    task_one = do_task(line, False)
+    print(task_one)
+    task_two = do_task(line, True)
+    print(task_two)
