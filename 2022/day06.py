d91017d3ef762fc4c05ac9bc906da1a164c99b04
Benjamin Packer <benjaminpacker16@gmail.com>
2022-12-07 13:37:30 +0000

    improved day 6

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,13 +1,10 @@
 with open('2022/data/input/day06.txt', 'r') as file:
     input = file.readline().strip()
 
-task_one = 0
-task_two = 0
-for i in range(0, len(input)):
-    if task_one == 0 and len(set(input[i:i+4])) == 4:
-        task_one = i + 4
-    if task_two == 0 and len(set(input[i:i+14])) == 14:
-        task_two = i + 14
+def do_task(distinct_characters):
+    for i in range(0, len(input)):
+        if len(set(input[i:i + distinct_characters])) == distinct_characters:
+            return i + distinct_characters
 
-print(f'Task One: {task_one}')
-print(f'Task Two: {task_two}')
+print(f'Task One: {do_task(4)}')
+print(f'Task Two: {do_task(14)}')
